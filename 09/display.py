2d78cedaaeed281c8caa980b194001c870e3e61f
Majda Trpkos <majda.trpkos@gmail.com>
2020-01-08 19:02:43 +0100

    Code enhancements

--- 09/display.py
+++ 09/display.py
@@ -19,7 +19,9 @@ image = Image.new('1', (width, height))
 draw = ImageDraw.Draw(image)
 
 
-font = ImageFont.load_default()
+# font = ImageFont.load_default()
+font = ImageFont.truetype('ARIALBD.TTF', 14)
+font_small = ImageFont.truetype('ARIALBD.TTF', 9)
 
 
 def clear():
@@ -34,8 +36,13 @@ if __name__ == "__main__":
     while True:
         clear()
 
+        CPU = subprocess.check_output("top -bn1 | grep load | awk '{printf \"CPU Load: %.2f\", $(NF-2)}'", shell = True )
+        MEM = subprocess.check_output("free -m | awk 'NR==2{printf \"Mem: %s/%sMB %.2f%%\", $3,$2,$3*100/$2 }'", shell = True )
+
         # DRAWING
-        draw.text((2, 2), "IOT Navrat", font=font, fill=255)
+        draw.text((2, 4), "IOT NAVRAT", font=font, fill=255)
+        draw.text((2, 28), CPU, font=font, fill=255)
+        draw.text((2, 44), MEM, font=font_small, fill=255)
 
         invalidate()
         time.sleep(0.1)
