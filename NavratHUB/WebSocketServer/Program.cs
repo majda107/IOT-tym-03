53eb7a20944909abb13159803ab24a87334a0233
majda107 <majda.trpkos@gmail.com>
2020-02-07 09:40:33 +0100

    Added hub solution

---
+++ NavratHUB/WebSocketServer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WebSocketServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
