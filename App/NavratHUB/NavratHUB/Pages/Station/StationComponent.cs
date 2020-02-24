124a5a62a8aa8154aa925fc960635e07485e422d
majda107 <majda.trpkos@gmail.com>
2020-02-24 11:54:09 +0100

    StationComponent base, HumidityComponent

---
+++ App/NavratHUB/NavratHUB/Pages/Station/StationComponent.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Components;
+
+namespace NavratHUB.Pages.Station
+{
+    public class StationComponent : ComponentBase
+    {
+        [Parameter]
+        public float Value { get; set; }
+    }
+}
